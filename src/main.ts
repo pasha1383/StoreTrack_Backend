f1ff76d707396c790d3c5f133aeb2500402c7bc7
pasha1383 <parsashadkam2017@gmail.com>
2025-08-09 13:43:36 +0330

    add auth part and login and register endpoint 2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ConfigService} from "@nestjs/config";
+import {ValidationPipe} from "@nestjs/common";
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+
+  const configService = app.get(ConfigService);
+  // Read the port from .env, defaulting to 3000 if not set
+  const port = configService.get<number>('APP_PORT', 3000);
+
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Strips away properties that are not defined in the DTO
+    forbidNonWhitelisted: true, // Throws an error if non-whitelisted properties are sent
+    transform: true, // Automatically transforms payload objects to DTO instances
+  }));
+  await app.listen(port);
 }
 bootstrap();
