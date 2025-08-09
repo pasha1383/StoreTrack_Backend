8dbe6de1a4f96b92c01b5f565f743608955996d5
pasha1383 <parsashadkam2017@gmail.com>
2025-08-09 16:49:12 +0330

    fix the bug of project

--- src/alerts/alerts.module.ts
+++ src/alerts/alerts.module.ts
@@ -2,9 +2,18 @@
 import { Module } from '@nestjs/common';
 import { AlertsService } from './alerts.service';
 import { ReportsModule } from '../reports/reports.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Product} from "../products/entities/product.entity";
+import {OrderItem} from "../orders/entities/order-item.entity";
+import {Report} from "../reports/entities/report.entity";
+import {ReportsService} from "../reports/reports.service";
 
 @Module({
-    imports: [ReportsModule],
-    providers: [AlertsService],
+    imports: [
+        ReportsModule,
+        TypeOrmModule.forFeature([Product,Report,OrderItem]),
+    ],
+    providers: [AlertsService,ReportsService],
+    exports: [AlertsService],
 })
 export class AlertsModule {}
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Product} from "../products/entities/product.entity";
+import {Report} from "./entities/report.entity";
+import {OrderItem} from "../orders/entities/order-item.entity";
 
 @Module({
+  imports: [
+      TypeOrmModule.forFeature([Product,Report,OrderItem]),
+  ],
   controllers: [ReportsController],
   providers: [ReportsService],
+  exports:[ReportsService]
 })
 export class ReportsModule {}
